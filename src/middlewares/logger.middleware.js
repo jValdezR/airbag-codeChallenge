1b0510ba0c62e8c1628a93d3b7180af329dd32c9
Jesus Valdez <jvaldezr@A2338.local>
2023-11-09 03:47:01 -0600

    better comments

--- src/middlewares/logger.middleware.js
+++ src/middlewares/logger.middleware.js
@@ -1,25 +1,26 @@
-const {logger : Logger} = require('../database/models/index');
+const { Logger } = require('../database/models/index');
 
-class LoggerMiddleware{
+class LoggerMiddleware {
+  // Middleware to save request information to the database
   async saveRequest(req, res, next) {
+    // Extract relevant request information
     const requestInfo = {
       method: req.method,
       url: req.url,
       timestamp: new Date(),
     };
+
     try {
+      // Save the request information to the database using the Logger model
       await Logger.create(requestInfo);
-      next();
+      next(); // Continue processing the request
     } catch (error) {
+      // Handle database insertion error
       res.status(500).json({
-        message: 'Internal Server Error'
+        message: 'Internal Server Error',
       });
     }
-    
-  
   }
 }
-
-
 
 module.exports = new LoggerMiddleware();
