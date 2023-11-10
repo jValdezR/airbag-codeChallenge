1250f8c36494a33997682d14b42c4fbf1dc92313
Jesus Valdez <jvaldezr@A2338.local>
2023-11-10 02:26:23 -0600

    security middleware implemented x2

---
+++ src/middlewares/security.middleware.js
@@ -0,0 +1,15 @@
+const {verifyApiKey} = require('../validators/security.validator');
+
+class SecurityMiddleware {
+  async verifyApiKey(req, res, next) {
+    try {
+      const headers = {...req.headers};
+      await verifyApiKey().validateAsync(headers);
+      next();
+    } catch (error) {
+      res.status(401).json({ status: false, message: 'Unauthorized, invalid x-api-key', data: null });
+    }
+  }
+}
+
+module.exports = new SecurityMiddleware();
